312ba5d2d94976f02160dafb168e61b8a41c1632
sandeshagarwal <sandeshagarwal10@gmail.com>
2020-04-26 12:31:52 +0530

    P2 - Addition Program

---
+++ Program 2.cs
@@ -0,0 +1,15 @@
+using System;
+namespace Mainw{
+    class Main2{
+        static void Main (String[] args)
+        {
+            int a =10;
+            int b =20;
+            int c = a+b;
+            
+            Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c); // This is in C# {0}{1}{2} is used to define the variables
+            Console.WriteLine("a = "+ a +" b = "+ b +" c = "+ c); // This is similiar to Java
+
+        }
+    }
+}
